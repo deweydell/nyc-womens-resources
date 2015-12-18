c99f15a63879dce92e23cdcbb13b9166b14f7534
Nicola Beuscher & Lateesha Thomas <pair+deweydell.lateesha@devbootcamp.com>
2015-12-18 12:02:14 -0500

    Add initialize method for listing class

--- listing.rb
+++ listing.rb
@@ -1,10 +1,17 @@
 class Listing
+  BOROUGHS = ["brooklyn", "queens", "staten_island", "manhattan", "bronx"]
+  attr_reader :name, :borough, :address, :phone, :url, :description
+
   def initialize(args = {})
+    @name = args.fetch(:organizationname, "")
     @borough = parse_args_for_boroughs(args)
+    @address = args.fetch(:location_1, "")
+    @phone = args.fetch(:phone, "")
+    @url = args.fetch(:url, "")
+    @description = args.fetch(:description, "")
   end
 
   def parse_args_for_boroughs(args)
-    boroughs = ["brooklyn", "queens", "staten_island", "manhattan", "bronx"]
-    args.select { |col, val| boroughs.include?(col) && val == "Y"  }.map { |col, val| col }
+    args.select { |col, val| BOROUGHS.include?(col) && val == "Y" }.map { |col, val| col }
   end
 end
