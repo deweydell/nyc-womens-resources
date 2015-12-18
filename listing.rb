78ffe97e472073dfdeb1d0a89512c29fe1aebf83
Nicola Beuscher & Lateesha Thomas <pair+deweydell.lateesha@devbootcamp.com>
2015-12-18 12:18:25 -0500

    Update listing class

--- listing.rb
+++ listing.rb
@@ -1,17 +1,25 @@
+require 'pry'
+require 'open-uri'
+require "JSON"
+
 class Listing
   BOROUGHS = ["brooklyn", "queens", "staten_island", "manhattan", "bronx"]
   attr_reader :name, :borough, :address, :phone, :url, :description
 
   def initialize(args = {})
-    @name = args.fetch(:organizationname, "")
+    @name = args.fetch("organizationname", " ")
     @borough = parse_args_for_boroughs(args)
-    @address = args.fetch(:location_1, "")
-    @phone = args.fetch(:phone, "")
-    @url = args.fetch(:url, "")
-    @description = args.fetch(:description, "")
+    @phone = args.fetch("phone", " ")
+    @url = args.fetch("url", " ")
+    @description = args.fetch("description", " ")
   end
 
   def parse_args_for_boroughs(args)
     args.select { |col, val| BOROUGHS.include?(col) && val == "Y" }.map { |col, val| col }
   end
 end
+
+listings = open('http://data.cityofnewyork.us/resource/pqg4-dm6b.json?Aging=Y')
+aging_listings = JSON.parse(listings.read)
+
+p query = Listing.new(aging_listings.first)
