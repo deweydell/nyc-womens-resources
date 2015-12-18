6b308b454c6b39248c5ae47c1849a78e8e6af237
Chris Guard & Jessie Richardson <pair+cguard90.jesskrich@devbootcamp.com>
2015-12-18 12:35:09 -0500

    update tasks on View class

--- View_class.rb
+++ View_class.rb
@@ -1,16 +1,27 @@
 class View
   attr_accessor :category_to_search
 
+  require 'pry'
+
   def welcome_message
     print "Welcome to NYC Women's Resource Finder"
   end
 
-  def search_prompt
-    print "What Category would you like to search? \n"
-      CATEGORY_CODES.each do |category_array|
-          print "#{category_array[0]} \n"
+  def search_prompt(categories)
+    print "What Category number would you like to search? \n"
+    counter = 1
+      categories.each do |category_array|
+          print "#{counter}: #{category_array[0]} \n"
+          counter += 1
       end
-    category_to_search = gets.chomp
+    category_to_search = gets.chomp.to_i
+    if !category_to_search.is_a?(Integer)
+      raise ArgumentError.new("Please select a number \n")
+      search_prompt
+    end
   end
 
+  def convert_to_index(category_to_search) #move to controller
+    category_to_search -= 1
+  end
 end
