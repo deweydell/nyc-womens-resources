c7e0f469fb3e43241064bbd5afeb809195f378a8
Nicola Beuscher & Lateesha Thomas <pair+deweydell.lateesha@devbootcamp.com>
2015-12-18 11:51:58 -0500

    Add initial listing class and controller ruby files

---
+++ controller.rb
@@ -0,0 +1,30 @@
+CATEGORY_CODES = [["Aging", "aging"]
+"Anti-Discrimination/HR", ""
+"Arts", ""
+"Business", ""
+"Childcare and parent", ""
+["Community Service/Volunteer", "volunteerism"]
+"Counseling", ""
+"Disability", ""
+"Domestic Violence", ""
+"education", ""
+"Employment", ""
+"Homelessness", ""
+"Housing", ""
+"Immigration", ""
+"Legal", ""
+"LGBT", ""
+"Personal Finance", ""
+"Professional Association", ""
+"Veterans", ""
+"Victims", ""
+"Women's Groups", ""
+"Youth Services"] ""
+
+CATEGORY_CODES[user_input.to_i][1]
+
+build_query()
+
+def build_query(category_code)
+  ""
+end
