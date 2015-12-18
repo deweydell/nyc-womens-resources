39106e47a0677ae7bf3ebf9e86cbe9d5b34ca20d
Chris Guard & Jessie Richardson <pair+cguard90.jesskrich@devbootcamp.com>
2015-12-18 13:40:52 -0500

    Create class for Controller

--- controller.rb
+++ controller.rb
@@ -1,17 +1,26 @@
 require_relative 'View_class'
+class Controller
+  attr_accessor :category_index
+  attr_reader :viewer
+
+  def initialize
+    @viewer = View.new
+    @category_index = 0
+    main_runner
+  end
 
 CATEGORY_CODES = [["Aging", "aging"],
 ["Anti-Discrimination/HR", "anti_discrimination_human_rights"],
 ["Arts", "anti_discrimination_human_rights"],
 ["Business", "business"],
 ["Childcare and parent", "child_care_parent_information"],
-[["Community Service/Volunteer", "community_service_volunteerism"],],
+["Community Service/Volunteer", "community_service_volunteerism"],
 ["Counseling", "counseling_support_groups"],
 ["Disability", " disabilities"],
 ["Domestic Violence", "domestic_violence"],
 ["education", "education"],
 ["Employment", "employment_job_training"],
-["Health", "health"]
+["Health", "health"],
 ["Homelessness", "homelessness"],
 ["Housing", "housing"],
 ["Immigration", "immigration"],
@@ -24,12 +33,27 @@ CATEGORY_CODES = [["Aging", "aging"],
 ["Women's Groups", "women_s_groups"],
 ["Youth Services", "youth_services"]]
 
+  def main_runner
+    viewer.welcome_message
+    user_index_choice = self.get_category_from_user
+    # do something with that number
+    puts "THE NUMBER THAT THE USER CHOSE WAS: #{user_index_choice}"
+  end
+
+
+  def get_category_from_user
+    viewer.search_prompt(CATEGORY_CODES) - 1
+    # convert_to_index = viewer.category_to_search
+  end
 # CATEGORY_CODES[user_input.to_i][1]
 
 # build_query()
 
-def build_query(category_code)
-  response = open("http://data.cityofnewyork.us/resource/pqg4-dm6b.json?#{category_code}=Y")
-  JSON.parse(response.read)
+  def build_query(category_code)
+    response = open("http://data.cityofnewyork.us/resource/pqg4-dm6b.json?#{category_code}=Y")
+    JSON.parse(response.read)
+  end
+
 end
+
 #this method will return the array of hash data in the specific category
